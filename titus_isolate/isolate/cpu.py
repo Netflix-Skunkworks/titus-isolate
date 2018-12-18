a6bd8656e355fa68f1116997d48e37fb5e9067cd
Gabriel Hartmann <gabriel.hartmann@gmail.com>
2018-12-18 09:56:59 -0800

    Remove dead greedy allocation code

--- titus_isolate/isolate/cpu.py
+++ titus_isolate/isolate/cpu.py
@@ -94,7 +94,6 @@ def free_threads(cpu, workload_id, workload_insertion_times={}):
     if len(curr_placement_vectors) == 0:
         raise Exception("Cannot free a workload from an empty CPU")
 
-
     requested_cus = [len(curr_ids_per_workload[wid]) if wid != workload_id else 0 for wid in ordered_workload_ids]
 
     new_placement_vectors = optimize_ip(requested_cus, n_compute_units, n_packages, curr_placement_vectors, verbose=True)
@@ -117,41 +116,3 @@ def free_threads(cpu, workload_id, workload_insertion_times={}):
             new_used_threads.append(t)
 
     return new_used_threads
-
-
-def assign_threads_greedy(cpu, workload):
-    thread_count = workload.get_thread_count()
-    claimed_threads = []
-
-    if thread_count == 0:
-        return claimed_threads
-
-    log.info("Assigning '{}' thread(s) to workload: '{}'".format(workload.get_thread_count(), workload.get_id()))
-
-    if is_cpu_full(cpu):
-        raise ValueError("Cannot assign workload: '{}' to full CPU.".format(workload.get_id()))
-
-    package = cpu.get_emptiest_package()
-
-    while thread_count > 0 and len(package.get_empty_threads()) > 0:
-        core = get_emptiest_core(package)
-        empty_threads = core.get_empty_threads()[:thread_count]
-
-        for empty_thread in empty_threads:
-            log.debug("Claiming package:core:thread '{}:{}:{}' for workload '{}'".format(
-                package.get_id(), core.get_id(), empty_thread.get_id(), workload.get_id()))
-            empty_thread.claim(workload.get_id())
-            claimed_threads.append(empty_thread)
-            thread_count -= 1
-
-    return claimed_threads + assign_threads(cpu, Workload(workload.get_id(), thread_count, workload.get_type()))
-
-
-def free_threads_greedy(cpu, workload_id):
-    freed_threads = []
-    for t in cpu.get_threads():
-        if t.get_workload_id() == workload_id:
-            t.free()
-            freed_threads.append(t)
-
-    return freed_threads
