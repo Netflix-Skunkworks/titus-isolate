fdd361eff9095268401b6cb1f0f0adaf32975f9e
Benoit Rostykus <brostykus@netflix.com>
2018-12-19 20:14:11 -0800

    Documentation, reintroduce greedy for later

--- titus_isolate/isolate/cpu.py
+++ titus_isolate/isolate/cpu.py
@@ -13,6 +13,10 @@ def assign_threads(cpu, workload, workload_insertion_times={}):
     """
     Use the integer -program solver to find the optimal static placement
     when adding the given workload on the cpu.
+
+    `workload_insertion_times` is a workload_id --> integer dict
+    indicating unix timestamps at which workloads currently running on the cpu
+    have been placed.
     """
     n_packages = len(cpu.get_packages())
     n_compute_units = len(cpu.get_threads())
@@ -116,3 +120,41 @@ def free_threads(cpu, workload_id, workload_insertion_times={}):
             new_used_threads.append(t)
 
     return new_used_threads
+
+
+def assign_threads_greedy(cpu, workload):
+    thread_count = workload.get_thread_count()
+    claimed_threads = []
+
+    if thread_count == 0:
+        return claimed_threads
+
+    log.info("Assigning '{}' thread(s) to workload: '{}'".format(workload.get_thread_count(), workload.get_id()))
+
+    if is_cpu_full(cpu):
+        raise ValueError("Cannot assign workload: '{}' to full CPU.".format(workload.get_id()))
+
+    package = cpu.get_emptiest_package()
+
+    while thread_count > 0 and len(package.get_empty_threads()) > 0:
+        core = get_emptiest_core(package)
+        empty_threads = core.get_empty_threads()[:thread_count]
+
+        for empty_thread in empty_threads:
+            log.debug("Claiming package:core:thread '{}:{}:{}' for workload '{}'".format(
+                package.get_id(), core.get_id(), empty_thread.get_id(), workload.get_id()))
+            empty_thread.claim(workload.get_id())
+            claimed_threads.append(empty_thread)
+            thread_count -= 1
+
+    return claimed_threads + assign_threads(cpu, Workload(workload.get_id(), thread_count, workload.get_type()))
+
+
+def free_threads_greedy(cpu, workload_id):
+    freed_threads = []
+    for t in cpu.get_threads():
+        if t.get_workload_id() == workload_id:
+            t.free()
+            freed_threads.append(t)
+
+    return freed_threads
