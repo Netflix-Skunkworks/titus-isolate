f3433867c83cc115089ab25d82e230dbd737a613
Gabriel Hartmann <gabriel.hartmann@gmail.com>
2018-12-28 10:05:31 -0800

    Remove BPF PMC metrics code, as this mechanism is flaky in reality

--- titus_isolate/metrics/pmc_subscriber.py
+++
@@ -1,9 +0,0 @@
-import collections
-
-
-class PmcSubscriber:
-    def __init__(self):
-        self.q = collections.deque()
-
-    def handle_nowait(self, metric):
-        self.q.appendleft(metric)
