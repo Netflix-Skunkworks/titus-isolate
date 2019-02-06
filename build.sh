9c3834d1b7970fa0dd31dc06b125c5a89527c268
Sargun Dhillon <sargun@sargun.me>
2019-02-06 15:42:54 -0800

    Use git describe to generate the information after date (#69)
    * Use git describe to generate the information after date
    
    git describe includes information, such as the number of commits
    This also splits the build script into a separate .sh file rather
    than inline in buildkite.
    
    * Tag exact tags correctly

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -x
+set -euo pipefail
+DERIVED_VERSION=$(date +%Y%m%d).$(git describe --tags --long | sed s/-/./g)
+TITUS_ISOLATE_VERSION=$(git describe --exact-match --tags || echo $DERIVED_VERSION)
+
+echo "Building version ${TITUS_ISOLATE_VERSION}"
+docker run --rm -e TITUS_ISOLATE_VERSION=${TITUS_ISOLATE_VERSION} -v $PWD:/src deb:latest
+mv *.deb titus-isolate_latest.deb
+
