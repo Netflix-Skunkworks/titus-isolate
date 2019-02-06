683c7e30c825826f50d74270c576827e212b319f
Gabriel Hartmann <gabriel.hartmann@gmail.com>
2019-02-05 16:04:37 -0800

    Add method to compute cpu usage per workload

--- tests/monitor/test_workload_perf_mon.py
+++ tests/monitor/test_workload_perf_mon.py
@@ -1,3 +1,4 @@
+import datetime
 import unittest
 import uuid
 from unittest.mock import MagicMock
@@ -6,6 +7,7 @@ from titus_isolate.docker.constants import STATIC
 from titus_isolate.model.workload import Workload
 from titus_isolate.monitor.cgroup_metrics_provider import CgroupMetricsProvider
 from titus_isolate.monitor.cpu_usage import CpuUsage, CpuUsageSnapshot
+from titus_isolate.monitor.workload_monitor_manager import DEFAULT_SAMPLE_FREQUENCY_SEC
 from titus_isolate.monitor.workload_perf_mon import WorkloadPerformanceMonitor, TIMESTAMP
 
 
@@ -14,10 +16,10 @@ class TestWorkloadPerfMon(unittest.TestCase):
     def test_single_sample(self):
         workload = Workload(uuid.uuid4(), 2, STATIC)
         metrics_provider = CgroupMetricsProvider(workload)
-        perf_mon = WorkloadPerformanceMonitor(metrics_provider)
+        perf_mon = WorkloadPerformanceMonitor(metrics_provider, DEFAULT_SAMPLE_FREQUENCY_SEC)
 
         # Initial state should just be an empty timestamps buffer
-        buffers = perf_mon.get_raw_buffers()
+        buffers = perf_mon.get_buffers()
         self.assertEqual(1, len(buffers))
         self.assertEqual(0, len(buffers[TIMESTAMP]))
 
@@ -27,11 +29,11 @@ class TestWorkloadPerfMon(unittest.TestCase):
 
         # Mock reporting metrics on a single hardware thread
         cpu_usage = CpuUsage(pu_id=0, user=100, system=50)
-        snapshot = CpuUsageSnapshot(timestamp=1000, rows=[cpu_usage])
+        snapshot = CpuUsageSnapshot(timestamp=datetime.datetime.now(), rows=[cpu_usage])
         metrics_provider.get_cpu_usage = MagicMock(return_value=snapshot)
         perf_mon.sample()
 
-        buffers = perf_mon.get_raw_buffers()
+        buffers = perf_mon.get_buffers()
         self.assertEqual(2, len(buffers))
         self.assertEqual(1, len(buffers[TIMESTAMP]))
 
@@ -39,3 +41,27 @@ class TestWorkloadPerfMon(unittest.TestCase):
         self.assertEqual(1, len(buffer))
         self.assertEqual(cpu_usage.user + cpu_usage.system, buffer[0])
 
+    def test_cpu_usage(self):
+        workload = Workload(uuid.uuid4(), 2, STATIC)
+        metrics_provider = CgroupMetricsProvider(workload)
+        perf_mon = WorkloadPerformanceMonitor(metrics_provider, DEFAULT_SAMPLE_FREQUENCY_SEC)
+
+        # Sample metrics
+        first = datetime.datetime(2019, 2, 5, second=0)
+        cpu_usage = CpuUsage(pu_id=0, user=100, system=50)
+        snapshot = CpuUsageSnapshot(timestamp=first, rows=[cpu_usage])
+        metrics_provider.get_cpu_usage = MagicMock(return_value=snapshot)
+        perf_mon.sample()
+
+        simulated_cpu_time = 1_000_000_000
+
+        # Sample again
+        second = datetime.datetime(2019, 2, 5, second=DEFAULT_SAMPLE_FREQUENCY_SEC)
+        cpu_usage = CpuUsage(pu_id=0, user=100 + simulated_cpu_time, system=50)
+        snapshot = CpuUsageSnapshot(timestamp=second, rows=[cpu_usage])
+        metrics_provider.get_cpu_usage = MagicMock(return_value=snapshot)
+        perf_mon.sample()
+
+        cpu_usage = perf_mon.get_cpu_usage(DEFAULT_SAMPLE_FREQUENCY_SEC)
+        self.assertEqual(1, len(cpu_usage))
+        self.assertEqual(simulated_cpu_time / (DEFAULT_SAMPLE_FREQUENCY_SEC * 1_000_000_000), cpu_usage['0'])
--- titus_isolate/monitor/workload_perf_mon.py
+++ titus_isolate/monitor/workload_perf_mon.py
@@ -9,8 +9,9 @@ TIMESTAMP = "timestamp"
 
 class WorkloadPerformanceMonitor:
 
-    def __init__(self, metrics_provider):
+    def __init__(self, metrics_provider, sample_frequency_sec):
         self.__metrics_provider = metrics_provider
+        self.__sample_frequency_sec = sample_frequency_sec
         self.__buffer_lock = Lock()
         self.__buffers = {
             TIMESTAMP: collections.deque([], MAX_COUNTER_BUFFER_SIZE)
@@ -19,9 +20,40 @@ class WorkloadPerformanceMonitor:
     def get_workload(self):
         return self.__metrics_provider.get_workload()
 
-    def get_raw_buffers(self):
+    def get_buffers(self):
         with self.__buffer_lock:
-            return self.__buffers
+            copy = {}
+            for key, buffer in self.__buffers.items():
+                copy[key] = list(buffer)
+            return copy
+
+    def get_cpu_usage(self, seconds):
+        sample_count = int(seconds) // self.__sample_frequency_sec
+
+        buffers = self.get_buffers()
+        end = len(buffers[TIMESTAMP]) - 1
+        start = end - sample_count
+
+        # Insufficient samples to satisfy request
+        if start < 0:
+            log.debug(
+                "Insufficient samples to calculate cpu usage over the last '{}' seconds for workload: '{}'".format(
+                seconds, self.get_workload().get_id()))
+            return None
+
+        end_time = buffers[TIMESTAMP][end]
+        start_time = buffers[TIMESTAMP][start]
+        diff = end_time - start_time
+        wall_time_ns = diff.total_seconds() * 1000000000
+
+        cpu_usage = collections.OrderedDict()
+        for pu_id, buffer in sorted(buffers.items()):
+            if pu_id == TIMESTAMP:
+                continue
+            cpu_time_ns = buffer[end] - buffer[start]
+            cpu_usage[pu_id] = cpu_time_ns / wall_time_ns
+
+        return cpu_usage
 
     def sample(self):
         cpu_usage_snapshot = self.__metrics_provider.get_cpu_usage()
@@ -37,7 +69,7 @@ class WorkloadPerformanceMonitor:
             for row in cpu_usage_snapshot.rows:
                 self.__buffers[str(row.pu_id)].append(int(row.user) + int(row.system))
 
-        log.debug("Took snapshot of metrics for workload: '{}'".format(self.get_workload().get_id()))
+            log.debug("Took snapshot of metrics for workload: '{}'".format(self.get_workload().get_id()))
 
     def __init_buffers(self, count):
         for x in range(count):
