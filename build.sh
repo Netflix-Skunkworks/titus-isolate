002fa3bc232f90288118a1b9962f61f17bd5c761
Sargun Dhillon <sargun@sargun.me>
2019-02-06 15:14:07 -0800

    Tag exact tags correctly

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -x
 set -euo pipefail
-export TITUS_ISOLATE_VERSION=$(date +%Y%m%d).$(git describe --tags --long | sed s/-/./g)
+DERIVED_VERSION=$(date +%Y%m%d).$(git describe --tags --long | sed s/-/./g)
+TITUS_ISOLATE_VERSION=$(git describe --exact-match --tags || echo $DERIVED_VERSION)
+
 echo "Building version ${TITUS_ISOLATE_VERSION}"
 docker run --rm -e TITUS_ISOLATE_VERSION=${TITUS_ISOLATE_VERSION} -v $PWD:/src deb:latest
 mv *.deb titus-isolate_latest.deb
