362a36c1f2921dca17fe55b9b4f695f6590dc1b3
Benoit Rostykus <brostykus@netflix.com>
2019-03-05 13:57:53 -0800

    Properly agg CPU data per workload

--- tests/monitor/test_workload_perf_mon.py
+++ tests/monitor/test_workload_perf_mon.py
@@ -1,14 +1,18 @@
-import datetime
+import calendar
+from collections import deque
+from datetime import datetime as dt, timedelta as td
 import unittest
 import uuid
 from unittest.mock import MagicMock
+
+import numpy as np
 
 from titus_isolate.docker.constants import STATIC
 from titus_isolate.model.workload import Workload
 from titus_isolate.monitor.cgroup_metrics_provider import CgroupMetricsProvider
 from titus_isolate.monitor.cpu_usage import CpuUsage, CpuUsageSnapshot
 from titus_isolate.monitor.workload_monitor_manager import DEFAULT_SAMPLE_FREQUENCY_SEC
-from titus_isolate.monitor.workload_perf_mon import WorkloadPerformanceMonitor, TIMESTAMP
+from titus_isolate.monitor.workload_perf_mon import WorkloadPerformanceMonitor
 
 
 class TestWorkloadPerfMon(unittest.TestCase):
@@ -19,53 +23,37 @@ class TestWorkloadPerfMon(unittest.TestCase):
         perf_mon = WorkloadPerformanceMonitor(metrics_provider, DEFAULT_SAMPLE_FREQUENCY_SEC)
 
         # Initial state should just be an empty timestamps buffer
-        buffers = perf_mon.get_buffers()
-        self.assertEqual(1, len(buffers))
-        self.assertEqual(0, len(buffers[TIMESTAMP]))
+        _, timestamps, buffers = perf_mon.get_buffers()
+        self.assertEqual(0, len(buffers))
+        self.assertEqual(0, len(timestamps))
 
         # Expect no change because no workload is really running and we haven't started mocking anything
         perf_mon.sample()
-        self.assertEqual(1, len(buffers))
+        self.assertEqual(0, len(buffers))
 
         # Mock reporting metrics on a single hardware thread
         cpu_usage = CpuUsage(pu_id=0, user=100, system=50)
-        snapshot = CpuUsageSnapshot(timestamp=datetime.datetime.now(), rows=[cpu_usage])
+        snapshot = CpuUsageSnapshot(timestamp=dt.now(), rows=[cpu_usage])
         metrics_provider.get_cpu_usage = MagicMock(return_value=snapshot)
         perf_mon.sample()
 
-        buffers = perf_mon.get_buffers()
-        self.assertEqual(2, len(buffers))
-        self.assertEqual(1, len(buffers[TIMESTAMP]))
+        _, timestamps, buffers = perf_mon.get_buffers()
+        self.assertEqual(1, len(buffers))
+        self.assertEqual(1, len(timestamps))
 
-        buffer = buffers[str(cpu_usage.pu_id)]
+        buffer = buffers[cpu_usage.pu_id]
         self.assertEqual(1, len(buffer))
         self.assertEqual(cpu_usage.user + cpu_usage.system, buffer[0])
 
-    def test_two_sample_monitors(self):
-        workload = Workload(uuid.uuid4(), 2, STATIC)
-        metrics_provider = CgroupMetricsProvider(workload)
-        perf_mon = WorkloadPerformanceMonitor(metrics_provider, DEFAULT_SAMPLE_FREQUENCY_SEC)
+    def test_monitor_cpu_usage_normalization(self):
+        to_ts = lambda d: calendar.timegm(d.timetuple())
 
-        # Sample metrics
-        first = datetime.datetime(2019, 2, 5, second=0)
-        cpu_usage = CpuUsage(pu_id=0, user=100, system=50)
-        snapshot = CpuUsageSnapshot(timestamp=first, rows=[cpu_usage])
-        metrics_provider.get_cpu_usage = MagicMock(return_value=snapshot)
-        perf_mon.sample()
-
-        simulated_cpu_time = 1_000_000_000
-
-        # Sample again
-        second = datetime.datetime(2019, 2, 5, second=DEFAULT_SAMPLE_FREQUENCY_SEC)
-        cpu_usage = CpuUsage(pu_id=0, user=100 + simulated_cpu_time, system=50)
-        snapshot = CpuUsageSnapshot(timestamp=second, rows=[cpu_usage])
-        metrics_provider.get_cpu_usage = MagicMock(return_value=snapshot)
-        perf_mon.sample()
-
-        cpu_usage = perf_mon.get_cpu_usage(DEFAULT_SAMPLE_FREQUENCY_SEC)
-        self.assertEqual(1, len(cpu_usage))
-        self.assertEqual(simulated_cpu_time / (DEFAULT_SAMPLE_FREQUENCY_SEC * 1_000_000_000), cpu_usage['0'])
-
-        processing_time = perf_mon.get_processing_time_ns(DEFAULT_SAMPLE_FREQUENCY_SEC)
-        self.assertEqual(1, len(processing_time))
-        self.assertEqual(simulated_cpu_time, processing_time['0'])
+        data = WorkloadPerformanceMonitor.normalize_data(
+            to_ts(dt(2019, 3, 5, 10, 15, 0)),
+            list(map(to_ts, [dt(2019, 3, 5, 10, 14, 30),
+             dt(2019, 3, 5, 10, 14, 11),
+             dt(2019, 3, 5, 10, 13, 28)])),
+            [deque([100, 200, 200], 100), deque([50, 300, 360], 100)]
+            )
+        self.assertEqual(360-50+200-100, data[-1])
+        self.assertEqual(59, np.sum(np.isnan(data).astype(np.int16)))
--- titus_isolate/monitor/cpu_usage_provider.py
+++ titus_isolate/monitor/cpu_usage_provider.py
@@ -5,4 +5,7 @@ class CpuUsageProvider:
 
     @abstractmethod
     def get_cpu_usage(self, seconds: int) -> dict:
+        """
+        Returns CPU usage per workload over the last `seconds` seconds
+        """
         pass
