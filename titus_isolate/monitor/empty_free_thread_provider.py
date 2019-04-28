4c624763d23a1c10aaf826ad2805b60547aa0db7
Gabriel Hartmann <gabriel.hartmann@gmail.com>
2019-04-28 13:20:57 -0700

    Fix empth free thread provider

--- titus_isolate/monitor/empty_free_thread_provider.py
+++ titus_isolate/monitor/empty_free_thread_provider.py
@@ -1,10 +1,18 @@
+from typing import Dict, List
+
 from titus_isolate import log
 from titus_isolate.model.processor.cpu import Cpu
+from titus_isolate.model.processor.thread import Thread
+from titus_isolate.model.workload import Workload
 from titus_isolate.monitor.free_thread_provider import FreeThreadProvider
 
 
 class EmptyFreeThreadProvider(FreeThreadProvider):
 
-    def get_free_threads(self, cpu: Cpu) -> list:
+    def get_free_threads(
+            self,
+            cpu: Cpu,
+            cpu_usage: Dict[str, float],
+            workload_map: Dict[str, Workload]) -> List[Thread]:
         log.info("All empty threads are free")
         return [t for t in cpu.get_threads() if len(t.get_workload_ids()) == 0]
