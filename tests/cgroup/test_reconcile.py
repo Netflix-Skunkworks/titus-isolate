0a118ff2a2b3c470de7f44481428e7eaecb3461f
Gabriel Hartmann <gabriel.hartmann@gmail.com>
2019-04-29 11:21:30 -0700

    Skip reconciliation when isolation work pending
    To avoid erroneous reconciliation failures we now skip reconciliation
    if a Thread setting a cpuset has not yet completed

--- tests/cgroup/test_reconcile.py
+++ tests/cgroup/test_reconcile.py
@@ -1,17 +1,24 @@
 import unittest
 from unittest.mock import MagicMock
 
+from tests.cgroup.mock_cgroup_manager import MockCgroupManager
 from tests.test_exit_handler import TestExitHandler
 from titus_isolate.cgroup.cgroup_manager import CgroupManager
 from titus_isolate.constants import RECONCILIATION_FAILURE_EXIT
 from titus_isolate.isolate.reconciler import Reconciler
 from titus_isolate.model.processor.config import get_cpu
 
+EXIT_HANDLER = TestExitHandler()
+
 
 class TestUtils(unittest.TestCase):
 
+    def setUp(self):
+        global EXIT_HANDLER
+        EXIT_HANDLER = TestExitHandler()
+
     def test_get_workloads(self):
-        reconciler = Reconciler(None, None)
+        reconciler = Reconciler(MockCgroupManager(), None)
         cpu = get_cpu(1, 4, 1)  # 1 socket, 4 cores, 1 thread per core
 
         threads = cpu.get_threads()
@@ -32,61 +39,52 @@ class TestUtils(unittest.TestCase):
         self.assertEqual([0, 2, 3], workloads["e"])
 
     def test_reconcile_no_workloads(self):
-        exit_handler = TestExitHandler()
         cpu = get_cpu()
 
-        reconciler = Reconciler(None, exit_handler)
+        reconciler = Reconciler(MockCgroupManager(), EXIT_HANDLER)
         reconciler.reconcile(cpu)
         self.__validate_state(
             reconciler,
-            exit_handler,
+            EXIT_HANDLER,
             exit_code=None,
-            unisolated_count=0,
-            missing_cpuset_count=0)
+            expected_success_count=1,
+            expected_skip_count=0)
 
     def test_reconcile_unisolated_workload(self):
-        exit_handler = TestExitHandler()
-
         # Simulate a workload which has been added but which isn't isolated yet
         cpu = get_cpu()
         cpu.get_threads()[0].claim("a")
 
         cgroup_manager = CgroupManager()
-        cgroup_manager.get_isolated_workload_ids = MagicMock(return_value=[])
+        cgroup_manager.has_pending_work = MagicMock(return_value=True)
 
-        reconciler = Reconciler(cgroup_manager, exit_handler)
+        reconciler = Reconciler(cgroup_manager, EXIT_HANDLER)
         reconciler.reconcile(cpu)
         self.__validate_state(
             reconciler,
-            exit_handler,
+            EXIT_HANDLER,
             exit_code=None,
-            unisolated_count=1,
-            missing_cpuset_count=0)
+            expected_success_count=0,
+            expected_skip_count=1)
 
     def test_reconcile_missing_cpuset(self):
-        exit_handler = TestExitHandler()
-
-        # Simulate a workload which has been added but which isn't isolated yet
         cpu = get_cpu()
         workload_id = "a"
         cpu.get_threads()[0].claim(workload_id)
 
         cgroup_manager = CgroupManager()
-        cgroup_manager.get_isolated_workload_ids = MagicMock(return_value=[workload_id])
+        cgroup_manager.get_cpuset = MagicMock(return_value=[])
 
-        reconciler = Reconciler(cgroup_manager, exit_handler)
+        reconciler = Reconciler(cgroup_manager, EXIT_HANDLER)
         reconciler.reconcile(cpu)
         self.__validate_state(
             reconciler,
-            exit_handler,
-            exit_code=None,
-            unisolated_count=0,
-            missing_cpuset_count=1)
+            EXIT_HANDLER,
+            exit_code=RECONCILIATION_FAILURE_EXIT,
+            expected_success_count=1,  # The mock exit handler doesn't actually kill anything, so we get a false success
+            expected_skip_count=0)
 
     def test_reconcile_cpuset_mismatch(self):
-        exit_handler = TestExitHandler()
-
-        # Simulate a workload which has been added but which isn't isolated yet
         cpu = get_cpu()
         workload_id = "a"
         cpu.get_threads()[0].claim(workload_id)
@@ -95,19 +93,16 @@ class TestUtils(unittest.TestCase):
         cgroup_manager.get_isolated_workload_ids = MagicMock(return_value=[workload_id])
         cgroup_manager.get_cpuset = MagicMock(return_value=[42])
 
-        reconciler = Reconciler(cgroup_manager, exit_handler)
+        reconciler = Reconciler(cgroup_manager, EXIT_HANDLER)
         reconciler.reconcile(cpu)
         self.__validate_state(
             reconciler,
-            exit_handler,
+            EXIT_HANDLER,
             exit_code=RECONCILIATION_FAILURE_EXIT,
-            unisolated_count=0,
-            missing_cpuset_count=0)
+            expected_success_count=1,  # The mock exit handler doesn't actually kill anything, so we get a false success
+            expected_skip_count=0)
 
     def test_reconcile_cpuset_match(self):
-        exit_handler = TestExitHandler()
-
-        # Simulate a workload which has been added but which isn't isolated yet
         cpu = get_cpu()
         workload_id = "a"
         cpu.get_threads()[0].claim(workload_id)
@@ -116,27 +111,31 @@ class TestUtils(unittest.TestCase):
         cgroup_manager.get_isolated_workload_ids = MagicMock(return_value=[workload_id])
         cgroup_manager.get_cpuset = MagicMock(return_value=[0])
 
-        reconciler = Reconciler(cgroup_manager, exit_handler)
+        reconciler = Reconciler(cgroup_manager, EXIT_HANDLER)
         reconciler.reconcile(cpu)
         self.__validate_state(
             reconciler,
-            exit_handler,
+            EXIT_HANDLER,
             exit_code=None,
-            unisolated_count=0,
-            missing_cpuset_count=0)
+            expected_success_count=1,
+            expected_skip_count=0)
 
-    def __validate_state(self, reconciler, exit_handler, exit_code, unisolated_count, missing_cpuset_count):
+    def __validate_state(self, reconciler, exit_handler, exit_code, expected_success_count, expected_skip_count):
         actual_exit_code = exit_handler.last_code
-        self.assertEqual(exit_code, exit_handler.last_code, "Expected exit_code: '{}', found: '{}'".format(exit_code, actual_exit_code))
+        self.assertEqual(
+            exit_code,
+            exit_handler.last_code,
+            "Expected exit_code: '{}', found: '{}'".format(exit_code, actual_exit_code))
 
-        actual_unisolated_count = reconciler.get_unisolated_workload_count()
-        self.assertEqual(unisolated_count, reconciler.get_unisolated_workload_count(), "Expected unisolated_count: '{}', found: '{}'".format(unisolated_count, actual_unisolated_count))
+        actual_success_count = reconciler.get_success_count()
+        self.assertEqual(
+            expected_success_count,
+            actual_success_count,
+            "Expected success count: '{}', found: '{}'".format(expected_success_count, actual_success_count))
 
-        actual_missing_cpuset_count = reconciler.get_missing_cpuset_count()
-        self.assertEqual(missing_cpuset_count, reconciler.get_missing_cpuset_count(), "Expected missing cpuset count: '{}', found: '{}'".format(missing_cpuset_count, actual_missing_cpuset_count))
-
-        expected_total_warning_count = unisolated_count + missing_cpuset_count
-        actual_total_warning_count = reconciler.get_total_warning_count()
-
-        self.assertEqual(unisolated_count + missing_cpuset_count, reconciler.get_total_warning_count(), "Expected total warning count: '{}', found: '{}'".format(expected_total_warning_count, actual_total_warning_count))
+        actual_skip_count = reconciler.get_skip_count()
+        self.assertEqual(
+            expected_skip_count,
+            actual_skip_count,
+            "Expected skip count: '{}', found: '{}'".format(expected_skip_count, actual_skip_count))
 
--- titus_isolate/isolate/reconciler.py
+++ titus_isolate/isolate/reconciler.py
@@ -2,33 +2,29 @@ from titus_isolate import log
 from titus_isolate.cgroup.cgroup_manager import CgroupManager
 from titus_isolate.constants import RECONCILIATION_FAILURE_EXIT
 from titus_isolate.exit_handler import ExitHandler
+from titus_isolate.metrics.constants import RECONCILE_SKIP_COUNT, RECONCILE_SUCCESS_COUNT
+from titus_isolate.metrics.metrics_reporter import MetricsReporter
 from titus_isolate.model.processor.cpu import Cpu
 
 
-class Reconciler:
+class Reconciler(MetricsReporter):
 
     def __init__(self, cgroup_manager: CgroupManager, exit_handler: ExitHandler):
         self.__cgroup_manager = cgroup_manager
         self.__exit_handler = exit_handler
-        self.__unisolated_workload_count = 0
-        self.__missing_cpuset_count = 0
+        self.__reg = None
+        self.__skip_count = 0
+        self.__success_count = 0
 
     def reconcile(self, cpu: Cpu):
+        if self.__cgroup_manager.has_pending_work():
+            log.warning("Skipping reconciliation as some isolation work is still pending.")
+            self.__skip_count += 1
+            return
+
         workloads = self.get_workloads(cpu)
         for w_id, t_ids in workloads.items():
-            isolated_workload_ids = self.__cgroup_manager.get_isolated_workload_ids()
-            if w_id not in isolated_workload_ids:
-                log.warn("Workload: '{}' is not in isolated set: '{}' so unable to retrieve cpuset during reconciliation.".format(w_id, isolated_workload_ids))
-                self.__unisolated_workload_count += 1
-                continue
-
-            cpuset = self.__cgroup_manager.get_cpuset(w_id)
-            if cpuset is None:
-                log.warn("Workload likely exited during reconciliation: '{}'".format(w_id))
-                self.__missing_cpuset_count += 1
-                continue
-
-            cpuset = sorted(cpuset)
+            cpuset = sorted(self.__cgroup_manager.get_cpuset(w_id))
             t_ids = sorted(t_ids)
 
             if cpuset != t_ids:
@@ -39,14 +35,20 @@ class Reconciler:
                 log.info("Reconciliation has succeeded for workload: '{}', cpuset: {} == t_ids: {}".format(
                     w_id, cpuset, t_ids))
 
-    def get_unisolated_workload_count(self):
-        return self.__unisolated_workload_count
+        self.__success_count += 1
 
-    def get_missing_cpuset_count(self):
-        return self.__missing_cpuset_count
+    def get_skip_count(self):
+        return self.__skip_count
 
-    def get_total_warning_count(self):
-        return self.get_unisolated_workload_count() + self.get_missing_cpuset_count()
+    def get_success_count(self):
+        return self.__success_count
+
+    def set_registry(self, registry):
+        self.__reg = registry
+
+    def report_metrics(self, tags):
+        self.__reg.gauge(RECONCILE_SKIP_COUNT, tags).set(self.get_skip_count())
+        self.__reg.gauge(RECONCILE_SUCCESS_COUNT, tags).set(self.get_success_count())
 
     @staticmethod
     def get_workloads(cpu: Cpu):
