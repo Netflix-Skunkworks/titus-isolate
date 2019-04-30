90ab24f873af3b345a55f2d827d4023a249341db
Benoit Rostykus <brostykus@netflix.com>
2019-04-30 09:17:44 -0700

    Log allocator service task id

--- titus_isolate/allocate/constants.py
+++ titus_isolate/allocate/constants.py
@@ -9,5 +9,7 @@ CPU_ALLOCATOR = "cpu_allocator"
 TITUS_ISOLATE_CELL_HEADER = "X-Titus-Isolate-Cell"
 UNKNOWN_CELL = "unknown_cell"
 CELL = "cell"
+ALLOCATOR_SERVICE_TASK_ID = 'allocator_task_id' # task id of the service which did run a given allocation
+TITUS_TASK_ID = 'TITUS_TASK_ID'
 
 INSTANCE_ID = "instance_id"
