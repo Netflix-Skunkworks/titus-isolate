edf71879de2e51a2aefba22af9370ae79a15f898
Gabriel Hartmann <gabriel.hartmann@gmail.com>
2019-05-29 16:55:54 -0700

    Use systemd sockets for titus-isolate communication

--- titus_isolate/api/status.py
+++ titus_isolate/api/status.py
@@ -1,13 +1,35 @@
 import json
+import logging
 import time
 
+import docker
 from flask import Flask
 
 from titus_isolate import log
-from titus_isolate.config.constants import TITUS_ISOLATE_BLOCK_SEC, DEFAULT_TITUS_ISOLATE_BLOCK_SEC
+from titus_isolate.api.testing import is_testing
+from titus_isolate.cgroup.file_cgroup_manager import FileCgroupManager
+from titus_isolate.config.constants import TITUS_ISOLATE_BLOCK_SEC, DEFAULT_TITUS_ISOLATE_BLOCK_SEC, RESTART_PROPERTIES
+from titus_isolate.config.restart_property_watcher import RestartPropertyWatcher
+from titus_isolate.constants import FAILURE_EXIT_CODE
+from titus_isolate.event.create_event_handler import CreateEventHandler
+from titus_isolate.event.event_manager import EventManager
+from titus_isolate.event.free_event_handler import FreeEventHandler
+from titus_isolate.event.rebalance_event_handler import RebalanceEventHandler
+from titus_isolate.event.reconcile_event_handler import ReconcileEventHandler
+from titus_isolate.event.utils import get_current_workloads
 from titus_isolate.isolate.detect import get_cross_package_violations, get_shared_core_violations
+from titus_isolate.isolate.reconciler import Reconciler
+from titus_isolate.isolate.utils import get_fallback_allocator
+from titus_isolate.isolate.workload_manager import WorkloadManager
+from titus_isolate.metrics.keystone_event_log_manager import KeystoneEventLogManager
+from titus_isolate.metrics.metrics_manager import MetricsManager
+from titus_isolate.model.processor.config import get_cpu_from_env
+from titus_isolate.monitor.workload_monitor_manager import WorkloadMonitorManager
+from titus_isolate.predict.cpu_usage_predictor_manager import CpuUsagePredictorManager
+from titus_isolate.real_exit_handler import RealExitHandler
 from titus_isolate.utils import get_config_manager, get_workload_manager, get_event_manager, \
-    get_workload_monitor_manager
+    get_workload_monitor_manager, set_event_log_manager, start_periodic_scheduling, set_cpu_usage_predictor_manager, \
+    set_workload_monitor_manager, set_workload_manager, set_event_manager
 
 app = Flask(__name__)
 
@@ -88,3 +110,79 @@ def get_cpu_usage(workload_id, seconds, granularity):
         return json.dumps(cpu_usage[workload_id])
 
     return json.dumps({'unknown_workload_id': workload_id}), 404, {'ContentType': 'application/json'}
+
+
+if __name__ != '__main__' and not is_testing():
+    # Set the schedule library's logging level higher so it doesn't spam messages every time it schedules a task
+    logging.getLogger('schedule').setLevel(logging.WARN)
+
+    exit_handler = RealExitHandler()
+
+    log.info("Setting event log manager...")
+    event_log_manager = KeystoneEventLogManager()
+    set_event_log_manager(event_log_manager)
+
+    log.info("Watching property changes for restart...")
+    RestartPropertyWatcher(get_config_manager(), exit_handler, RESTART_PROPERTIES)
+
+    log.info("Modeling the CPU...")
+    cpu = get_cpu_from_env()
+
+    # Start period scheduling
+    log.info("Starting periodic event scheduling...")
+    start_periodic_scheduling()
+
+    # Start the cpu usage predictor manager
+    log.info("Setting up the cpu usage predictor manager...")
+    cpu_predictor_manager = CpuUsagePredictorManager()
+    set_cpu_usage_predictor_manager(cpu_predictor_manager)
+
+    # Start performance monitoring
+    log.info("Starting performance monitoring...")
+    workload_monitor_manager = WorkloadMonitorManager()
+    set_workload_monitor_manager(workload_monitor_manager)
+
+    # Setup the workload manager
+    log.info("Setting up the workload manager...")
+    cpu_allocator = get_fallback_allocator(get_config_manager())
+    log.info("Created Fallback CPU allocator with primary: '{}' and secondary: '{}".format(
+        cpu_allocator.get_primary_allocator().__class__.__name__,
+        cpu_allocator.get_secondary_allocator().__class__.__name__))
+    cgroup_manager = FileCgroupManager()
+    workload_manager = WorkloadManager(cpu=cpu, cgroup_manager=cgroup_manager, cpu_allocator=cpu_allocator)
+    set_workload_manager(workload_manager)
+
+    # Setup the event handlers
+    log.info("Setting up the Docker event handlers...")
+    create_event_handler = CreateEventHandler(workload_manager)
+    free_event_handler = FreeEventHandler(workload_manager)
+    rebalance_event_handler = RebalanceEventHandler(workload_manager)
+    reconciler = Reconciler(cgroup_manager, RealExitHandler())
+    reconcile_event_handler = ReconcileEventHandler(reconciler)
+    event_handlers = [create_event_handler, free_event_handler, rebalance_event_handler, reconcile_event_handler]
+
+    # Start event processing
+    log.info("Starting Docker event handling...")
+    event_manager = EventManager(docker.from_env().events(), event_handlers)
+    set_event_manager(event_manager)
+
+    # Report metrics
+    log.info("Starting metrics reporting...")
+    MetricsManager([
+        cgroup_manager,
+        event_log_manager,
+        event_manager,
+        reconciler,
+        workload_manager,
+        workload_monitor_manager])
+
+    # Initialize currently running containers as workloads
+    log.info("Isolating currently running workloads...")
+    for workload in get_current_workloads(docker.from_env()):
+        try:
+            workload_manager.add_workload(workload)
+        except:
+            log.exception("Failed to add currently running workload: '{}', maybe it exited.".format(workload.get_id()))
+
+    # Start processing events after adding running workloads to avoid processing a die event before we add a workload
+    event_manager.start_processing_events()
--- titus_isolate/real_exit_handler.py
+++ titus_isolate/real_exit_handler.py
@@ -1,12 +1,22 @@
 import os
+import signal
 import time
 
+from titus_isolate import log
 from titus_isolate.exit_handler import ExitHandler
 
 
 class RealExitHandler(ExitHandler):
 
+    def __init__(self):
+        signal.signal(signal.SIGINT, self.__sig_exit)
+        signal.signal(signal.SIGTERM, self.__sig_exit)
+
+    def __sig_exit(self, signum, frame):
+        log.info("Exiting due to signal: {} and frame: {}".format(signum, frame))
+        self.exit(signum)
+
     def exit(self, code):
         # Sleep for 1 second so log messages get flushed.
-        time.sleep(1)
+        time.sleep(0.1)
         os._exit(code)
