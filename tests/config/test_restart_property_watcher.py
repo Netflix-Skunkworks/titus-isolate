2e20e8bd922dd8385096b64dd0d842ba8443b72b
Gabriel Hartmann <gabriel.hartmann@gmail.com>
2019-05-31 15:26:36 -0700

    Remove legacy AB testing logic

--- tests/config/test_restart_property_watcher.py
+++ tests/config/test_restart_property_watcher.py
@@ -5,8 +5,7 @@ from tests.config.test_property_provider import TestPropertyProvider
 from tests.test_exit_handler import TestExitHandler
 from tests.utils import config_logs
 from titus_isolate.config.config_manager import ConfigManager
-from titus_isolate.config.constants import CPU_ALLOCATOR, NOOP, IP, AB_TEST, CPU_ALLOCATOR_A, CPU_ALLOCATOR_B, \
-    EC2_INSTANCE_ID, GREEDY
+from titus_isolate.config.constants import CPU_ALLOCATOR, NOOP, IP, GREEDY
 from titus_isolate.config.restart_property_watcher import RestartPropertyWatcher
 from titus_isolate.constants import ALLOCATOR_CONFIG_CHANGE_EXIT
 
@@ -51,29 +50,5 @@ class TestCpuAllocatorWatcher(unittest.TestCase):
 
         # titus-isolate should exit when the allocator changes
         property_provider.map[CPU_ALLOCATOR] = IP
-        watcher.detect_changes()
-        self.assertEqual(ALLOCATOR_CONFIG_CHANGE_EXIT, exit_handler.last_code)
-
-    def test_ab_classification_swap(self):
-        even_instance_id = 'i-0cfefd19c9a8db976'
-        property_provider = TestPropertyProvider(
-            {
-                CPU_ALLOCATOR: AB_TEST,
-                CPU_ALLOCATOR_A: NOOP,
-                CPU_ALLOCATOR_B: IP,
-                EC2_INSTANCE_ID: even_instance_id
-            })
-        exit_handler = TestExitHandler()
-        config_manager = ConfigManager(property_provider)
-        watcher = RestartPropertyWatcher(config_manager, exit_handler, [CPU_ALLOCATOR])
-
-        # No change yet
-        watcher.detect_changes()
-        self.assertEqual(None, exit_handler.last_code)
-
-        # Swap A and B to simulate instance classification change
-        # N.B. the ALLOCATOR_KEY and EC_INSTANCE_ID do NOT change
-        property_provider.map[CPU_ALLOCATOR_A] = IP
-        property_provider.map[CPU_ALLOCATOR_B] = NOOP
         watcher.detect_changes()
         self.assertEqual(ALLOCATOR_CONFIG_CHANGE_EXIT, exit_handler.last_code)
--- titus_isolate/isolate/utils.py
+++ titus_isolate/isolate/utils.py
@@ -1,4 +1,3 @@
-import datetime
 
 from titus_isolate import log
 from titus_isolate.allocate.fall_back_cpu_allocator import FallbackCpuAllocator
@@ -11,7 +10,7 @@ from titus_isolate.allocate.noop_reset_allocator import NoopResetCpuAllocator
 from titus_isolate.allocate.remote_cpu_allocator import RemoteCpuAllocator
 from titus_isolate.config.config_manager import ConfigManager
 from titus_isolate.config.constants import CPU_ALLOCATOR, CPU_ALLOCATORS, DEFAULT_ALLOCATOR, \
-    CPU_ALLOCATOR_A, CPU_ALLOCATOR_B, AB_TEST, EC2_INSTANCE_ID, IP, GREEDY, NOOP, FORECAST_CPU_IP, \
+    IP, GREEDY, NOOP, FORECAST_CPU_IP, \
     NOOP_RESET, FREE_THREAD_PROVIDER, DEFAULT_FREE_THREAD_PROVIDER, EMPTY, THRESHOLD, DEFAULT_TOTAL_THRESHOLD, \
     TOTAL_THRESHOLD, REMOTE, FALLBACK_ALLOCATOR, DEFAULT_FALLBACK_ALLOCATOR, OVERSUBSCRIBE, NAIVE
 from titus_isolate.monitor.empty_free_thread_provider import EmptyFreeThreadProvider
@@ -19,8 +18,6 @@ from titus_isolate.monitor.free_thread_provider import FreeThreadProvider
 from titus_isolate.monitor.oversubscribe_free_thread_provider import OversubscribeFreeThreadProvider
 from titus_isolate.monitor.threshold_free_thread_provider import ThresholdFreeThreadProvider
 from titus_isolate.utils import get_cpu_usage_predictor_manager
-
-BUCKETS = ["A", "B"]
 
 CPU_ALLOCATOR_NAME_TO_CLASS_MAP = {
     IP: IntegerProgramCpuAllocator,
@@ -49,18 +46,11 @@ def get_free_thread_provider(config_manager: ConfigManager) -> FreeThreadProvide
     return free_thread_provider
 
 
-def get_fallback_allocator(config_manager, hour=None) -> FallbackCpuAllocator:
-    if hour is None:
-        hour = datetime.datetime.utcnow().hour
-
+def get_fallback_allocator(config_manager) -> FallbackCpuAllocator:
     primary_alloc_str = config_manager.get_str(CPU_ALLOCATOR)
     secondary_alloc_str = config_manager.get_str(FALLBACK_ALLOCATOR, DEFAULT_FALLBACK_ALLOCATOR)
 
-    if primary_alloc_str == AB_TEST:
-        primary_allocator = __get_ab_allocator(config_manager, hour)
-    else:
-        primary_allocator = get_allocator(primary_alloc_str, config_manager)
-
+    primary_allocator = get_allocator(primary_alloc_str, config_manager)
     secondary_allocator = get_allocator(secondary_alloc_str, config_manager)
 
     return FallbackCpuAllocator(primary_allocator, secondary_allocator)
@@ -79,46 +69,3 @@ def get_allocator(allocator_str, config_manager):
         cpu_usage_predictor_manager=get_cpu_usage_predictor_manager(),
         config_manager=config_manager,
         free_thread_provider=free_thread_provider)
-
-
-def __get_ab_allocator(config_manager, hour):
-    a_allocator_str = config_manager.get_str(CPU_ALLOCATOR_A)
-    b_allocator_str = config_manager.get_str(CPU_ALLOCATOR_B)
-
-    a_allocator = get_allocator(a_allocator_str, config_manager)
-    b_allocator = get_allocator(b_allocator_str, config_manager)
-
-    bucket = get_ab_bucket(config_manager, hour)
-
-    if bucket not in BUCKETS:
-        log.error(
-            "Unexpected A/B bucket specified: '{}', falling back to default: '{}'".format(bucket, DEFAULT_ALLOCATOR))
-        return get_allocator("UNDEFINED_AB_BUCKET", config_manager)
-
-    return {
-        "A": a_allocator,
-        "B": b_allocator,
-    }[bucket]
-
-
-def get_ab_bucket(config_manager, hour):
-    instance_id = config_manager.get_str(EC2_INSTANCE_ID)
-    if instance_id is None:
-        log.error("Failed to find: '{}' in config manager, is the environment variable set?".format(EC2_INSTANCE_ID))
-        return "UNDEFINED"
-
-    # Take the last character of an instance id turn it into a number and determine whether it's odd or even.
-    # An instance id looks like this: i-0cfefd19c9a8db976
-    char = instance_id[-1]
-    bucket = _get_ab_bucket_int(char, hour)
-    if bucket == 0:
-        return "A"
-    else:
-        return "B"
-
-
-def _get_ab_bucket_int(char, hour):
-    instance_int = ord(char)
-    hour = int(hour / 6)
-    int_bucket = (instance_int + (hour % 2)) % 2
-    return int_bucket
