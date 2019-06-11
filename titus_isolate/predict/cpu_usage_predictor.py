dc780465124bb2b9384bbb35bf060ba6b02ab1b0
Benoit Rostykus <brostykus@netflix.com>
2019-06-11 11:36:19 -0700

    Upgrade GBRT v2

--- titus_isolate/predict/cpu_usage_predictor.py
+++ titus_isolate/predict/cpu_usage_predictor.py
@@ -1,11 +1,12 @@
 import os
+import re
 
 import numpy as np
 
 from titus_isolate import log
 from titus_isolate.model.workload import Workload
 
-from titus_optimize.data import Query, build_ts_features
+from titus_optimize.data import Query2, build_ts_features
 from titus_optimize.predictors import PredictorWithFilter
 
 
@@ -22,6 +23,7 @@ class CpuUsagePredictor:
         pf = CpuUsagePredictor.__load_model_from_file(model_path)
         self.__use_whitelist = use_whitelist
         self.__model = pf
+        self._img_name_regex = re.compile(r'^.+\:\d+/(.*)')
     
     def predict(self, workload: Workload, cpu_usage_last_hour: np.array, pred_env: PredEnvironment) -> float:
         image = workload.get_image()
@@ -29,15 +31,20 @@ class CpuUsagePredictor:
         valid_digest = False
         if cpu_usage_last_hour is None:
             cpu_usage_last_hour = np.full((60,), np.nan, dtype=np.float32)
+        image_name = None
         if len(tokens) == 2 and tokens[-1].startswith("sha256:"):
-            valid_digest = True
-            entry_point = workload.get_entrypoint()
-            if entry_point is None:
-                entry_point = ""
-            filter_key = "%s@%s" % (tokens[-1], entry_point)
+            m = self._img_name_regex.search(tokens[0])
+            if m is not None:
+                valid_digest = True
+                image_name = m.groups(0)[0]
+                entry_point = workload.get_entrypoint()[:1000]
+                if entry_point is None:
+                    entry_point = ""
+                filter_key = "%s@%s" % (tokens[-1], entry_point)
         if self.__use_whitelist and valid_digest and (filter_key not in self.__model.filter):
             # not in whitelist, predict without context features
-            q = Query(
+            q = Query2(
+                None, # image_name
                 None, # user
                 None, # app_name
                 workload.get_thread_count(),
@@ -51,7 +58,8 @@ class CpuUsagePredictor:
                 build_ts_features(cpu_usage_last_hour)
             )
         else:
-            q = Query(
+            q = Query2(
+                image_name,
                 workload.get_owner_email(),
                 workload.get_app_name(),
                 workload.get_thread_count(),
