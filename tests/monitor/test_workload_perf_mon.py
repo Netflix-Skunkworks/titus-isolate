Fix cpu normalization test

tests/monitor/test_workload_perf_mon.py
@@ -56,20 +56,33 @@ class TestWorkloadPerfMon(unittest.TestCase):
     def test_monitor_cpu_usage_normalization(self):
         to_ts = lambda d: calendar.timegm(d.timetuple())
 
+        timestamps = deque(map(to_ts,
+                               [dt(2019, 3, 5, 10, 13, 28),
+                                dt(2019, 3, 5, 10, 14, 11),
+                                dt(2019, 3, 5, 10, 14, 30)]))
+
         data = normalize_data(
-            to_ts(dt(2019, 3, 5, 10, 15, 0)),
-            deque(map(to_ts, [dt(2019, 3, 5, 10, 14, 30),
-             dt(2019, 3, 5, 10, 14, 11),
-             dt(2019, 3, 5, 10, 13, 28)])),
-            [deque([100, 200, 200], 100), deque([50, 300, 360], 100)]
-            )
-        self.assertEqual(360-50+200-100, data[-1])
-        self.assertEqual(59, np.sum(np.isnan(data).astype(np.int16)))
+            timestamps,
+            [deque([100, 200, 200], 100),
+             deque([50, 300, 360], 100)])
+
+        # Last data bucket
+        expected_processing_time = (360-300 + 200-200)
+        expected_duration_ns = (timestamps[-1] - timestamps[-2]) * 1000000000
+        expected_usage = expected_processing_time / expected_duration_ns
+        self.assertAlmostEqual(expected_usage, data[-1])
+
+        # Penultimate bucket
+        expected_processing_time = (300-50 + 200-100)
+        expected_duration_ns = (timestamps[-2] - timestamps[-3]) * 1000000000
+        expected_usage = expected_processing_time / expected_duration_ns
+        self.assertAlmostEqual(expected_usage, data[-2])
+
+        self.assertEqual(58, np.sum(np.isnan(data).astype(np.int16)))
 
         try:
             normalize_data(
-                to_ts(dt(2019, 3, 5, 10, 15, 0)),
                 deque([]),
                 [deque([]), deque([])])
-        except Exception:
+        except:
             self.fail("Should not raise on empty data.")

titus_isolate/monitor/utils.py
@@ -1,4 +1,6 @@
+import calendar
 from typing import Dict, List
+from datetime import datetime as dt
 
 from titus_isolate import log
 from titus_isolate.event.constants import STATIC
@@ -14,7 +16,6 @@ def get_free_cores(
         cpu: Cpu,
         workload_map: Dict[str, Workload],
         cpu_usage: Dict[str, float]) -> List[Core]:
-
     return [c for c in cpu.get_cores() if is_core_below_threshold(threshold, c, cpu_usage, workload_map)]
 
 
@@ -59,13 +60,14 @@ def is_core_below_threshold(
     return is_free
 
 
-def normalize_data(ts_snapshot, timestamps, buffers, num_buckets=60, bucket_size_secs=60):
+def normalize_data(timestamps, buffers, num_buckets=60, bucket_size_secs=60):
     proc_time = np.full((num_buckets,), np.nan, dtype=np.float32)
 
     if len(timestamps) == 0:
         return proc_time
 
-    ts_max = ts_snapshot
+    ts_max = timestamps[-1]
+
     for i in range(num_buckets):
         ts_min = ts_max - bucket_size_secs
 
@@ -91,7 +93,7 @@ def normalize_data(ts_snapshot, timestamps, buffers, num_buckets=60, bucket_size
         # this should be matching Atlas:
         time_diff_ns = (timestamps[slice_ts_max] - timestamps[slice_ts_min]) * 1000000000
         s = 0.0
-        for b in buffers: # sum across all cpus
+        for b in buffers:  # sum across all cpus
             s += b[slice_ts_max] - b[slice_ts_min]
         if time_diff_ns > 0:
             s /= time_diff_ns

titus_isolate/monitor/workload_perf_mon.py
@@ -35,10 +35,8 @@ class WorkloadPerformanceMonitor:
 
     def _get_cpu_buffers(self):
         with self.__snapshot_lock:
-            ts_snapshot = calendar.timegm(dt.utcnow().timetuple())
-
             if len(self.__snapshots) == 0:
-                return ts_snapshot, [], []
+                return [], []
 
             # The number of threads per sample is constant across all samples
             thread_count = len(self.__snapshots[0].cpu.rows)
@@ -50,8 +48,7 @@ class WorkloadPerformanceMonitor:
                 for row in res_snap.cpu.rows:
                     buffers[row.pu_id].append(int(row.user) + int(row.system))
 
-            return ts_snapshot, \
-                   np.array([calendar.timegm(t.timetuple()) for t in timestamps], dtype=np.int32), \
+            return np.array([calendar.timegm(t.timetuple()) for t in timestamps], dtype=np.int32), \
                    [list(e) for e in buffers]
 
     def get_cpu_usage(self, seconds, agg_granularity_secs=60):
@@ -59,5 +56,5 @@ class WorkloadPerformanceMonitor:
         if num_buckets > self.__max_buffer_size:
             raise Exception("Aggregation buffer too small to satisfy query.")
 
-        ts_snapshot, timestatamps, buffers = self._get_cpu_buffers()
-        return normalize_data(ts_snapshot, timestatamps, buffers, num_buckets, agg_granularity_secs)
+        timestamps, buffers = self._get_cpu_buffers()
+        return normalize_data(timestamps, buffers, num_buckets, agg_granularity_secs)