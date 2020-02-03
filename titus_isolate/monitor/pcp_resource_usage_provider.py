c67a81313a9c6d80851dd799d7ed610b849a9c1a
Gabriel Hartmann <gabriel.hartmann@gmail.com>
2020-02-03 10:12:58 -0800

    Only hold metrics snapshot lock when writing in memory object

--- titus_isolate/monitor/pcp_resource_usage_provider.py
+++ titus_isolate/monitor/pcp_resource_usage_provider.py
@@ -32,10 +32,9 @@ class PcpResourceUsageProvider:
         schedule.every(sample_interval_sec).seconds.do(self.__snapshot_usage_raw)
 
     def __snapshot_usage_raw(self) -> str:
-        with self.__lock:
-            try:
-                # pmrep -a /var/log/pcp/pmlogger/$(hostname)/ -S -60m -t 1m -y s -o csv -i .*titus-executor.*.service  cgroup.cpuacct.usage cgroup.memory.usage
-                snapshot_cmd_fmt = """ pmrep -a {0} \
+        try:
+            # pmrep -a /var/log/pcp/pmlogger/$(hostname)/ -S -60m -t 1m -y s -o csv -i .*titus-executor.*.service  cgroup.cpuacct.usage cgroup.memory.usage
+            snapshot_cmd_fmt = """ pmrep -a {0} \
                     -S -{1}s \
                     -T -0s \
                     -t {2}s \
@@ -45,17 +44,19 @@ class PcpResourceUsageProvider:
                     cgroup.cpuacct.usage \
                     cgroup.memory.usage """
 
-                cmd_str = snapshot_cmd_fmt.format(
-                    self.__archive_path,
-                    self.__relative_start_sec,
-                    self.__interval_sec)
+            cmd_str = snapshot_cmd_fmt.format(
+                self.__archive_path,
+                self.__relative_start_sec,
+                self.__interval_sec)
 
-                log.info('Snapshoting usage from pcp: {}'.format(cmd_str))
+            log.info('Snapshoting usage from pcp: {}'.format(cmd_str))
 
-                byte_array = subprocess.check_output(cmd_str, shell=True, timeout=self.__query_timeout_sec)
+            byte_array = subprocess.check_output(cmd_str, shell=True, timeout=self.__query_timeout_sec)
+
+            with self.__lock:
                 self.__raw_csv_snapshot = byte_array.decode('utf-8')
-            except:
-                log.exception("Failed to snapshot pcp raw data.")
+        except:
+            log.exception("Failed to snapshot pcp raw data.")
 
     def get_resource_usages(self) -> List[ResourceUsage]:
         with self.__lock:
