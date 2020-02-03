b123c315f931c9d20f7bb9f7e88ba32e6c2c73ef
Gabriel Hartmann <gabriel.hartmann@gmail.com>
2020-02-03 10:34:54 -0800

    Improve PCP metrics collection (#250)
    * Set explicit end time on pmrep call
    
    This avoids an infinite execution bug.  Addressed in pcp commit
    8b85f1d2ad7cc950717044f62593e0488fa28a20
    
    * Allow dynamic configuration of metrics query timeout
    
    * Only hold metrics snapshot lock when writing in memory object

--- titus_isolate/monitor/pcp_resource_usage_provider.py
+++ titus_isolate/monitor/pcp_resource_usage_provider.py
@@ -5,7 +5,7 @@ from typing import List
 import schedule
 
 from titus_isolate import log
-from titus_isolate.config.constants import DEFAULT_SAMPLE_FREQUENCY_SEC
+from titus_isolate.config.constants import DEFAULT_SAMPLE_FREQUENCY_SEC, DEFAULT_METRICS_QUERY_TIMEOUT_SEC
 from titus_isolate.monitor.resource_usage_provider import ResourceUsage
 from titus_isolate.monitor.utils import get_resource_usage, get_pcp_archive_path
 
@@ -16,25 +16,27 @@ class PcpResourceUsageProvider:
             self,
             relative_start_sec: int,
             interval_sec: int,
-            sample_interval: int = DEFAULT_SAMPLE_FREQUENCY_SEC):
+            sample_interval_sec: int = DEFAULT_SAMPLE_FREQUENCY_SEC,
+            query_timeout_sec: int = DEFAULT_METRICS_QUERY_TIMEOUT_SEC):
 
         self.__relative_start_sec = relative_start_sec
         self.__interval_sec = interval_sec
+        self.__query_timeout_sec = query_timeout_sec
         self.__interval_count = int(relative_start_sec / interval_sec)
         self.__archive_path = get_pcp_archive_path()
         self.__raw_csv_snapshot = None
         self.__lock = Lock()
         self.__snapshot_usage_raw()
 
-        log.info("Scheduling pcp metrics collecting every {} seconds".format(sample_interval))
-        schedule.every(sample_interval).seconds.do(self.__snapshot_usage_raw)
+        log.info("Scheduling pcp metrics collecting every {} seconds".format(sample_interval_sec))
+        schedule.every(sample_interval_sec).seconds.do(self.__snapshot_usage_raw)
 
     def __snapshot_usage_raw(self) -> str:
-        with self.__lock:
-            try:
-                # pmrep -a /var/log/pcp/pmlogger/$(hostname)/ -S -60m -t 1m -y s -o csv -i .*titus-executor.*.service  cgroup.cpuacct.usage cgroup.memory.usage
-                snapshot_cmd_fmt = """ pmrep -a {0} \
+        try:
+            # pmrep -a /var/log/pcp/pmlogger/$(hostname)/ -S -60m -t 1m -y s -o csv -i .*titus-executor.*.service  cgroup.cpuacct.usage cgroup.memory.usage
+            snapshot_cmd_fmt = """ pmrep -a {0} \
                     -S -{1}s \
+                    -T -0s \
                     -t {2}s \
                     -y s \
                     -o csv \
@@ -42,17 +44,19 @@ class PcpResourceUsageProvider:
                     cgroup.cpuacct.usage \
                     cgroup.memory.usage """
 
-                cmd_str = snapshot_cmd_fmt.format(
-                    self.__archive_path,
-                    self.__relative_start_sec,
-                    self.__interval_sec)
+            cmd_str = snapshot_cmd_fmt.format(
+                self.__archive_path,
+                self.__relative_start_sec,
+                self.__interval_sec)
 
-                log.info('Snapshoting usage from pcp: {}'.format(cmd_str))
+            log.info('Snapshoting usage from pcp: {}'.format(cmd_str))
 
-                byte_array = subprocess.check_output(cmd_str, shell=True, timeout=5)
+            byte_array = subprocess.check_output(cmd_str, shell=True, timeout=self.__query_timeout_sec)
+
+            with self.__lock:
                 self.__raw_csv_snapshot = byte_array.decode('utf-8')
-            except:
-                log.exception("Failed to snapshot pcp raw data.")
+        except:
+            log.exception("Failed to snapshot pcp raw data.")
 
     def get_resource_usages(self) -> List[ResourceUsage]:
         with self.__lock:
