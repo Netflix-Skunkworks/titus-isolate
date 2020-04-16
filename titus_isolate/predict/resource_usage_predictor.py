b28857ce2c02297399ec0e58e7f0f53dbc489076
Gabriel Hartmann <gabriel.hartmann@gmail.com>
2020-04-15 17:30:23 -0700

    Fix usage trimming
    Get the last 60 elements of the list, not the first 60 elements.  We
    want to discard the oldest usage elements, not the most recent

--- titus_isolate/predict/resource_usage_predictor.py
+++ titus_isolate/predict/resource_usage_predictor.py
@@ -91,7 +91,7 @@ class ResourceUsagePredictor(SimpleCpuPredictor):
     def __translate_usage(usages: Dict[str, List[float]]) -> dict:
         out_usage = {}
         for resource_name, values in usages.items():
-            out_usage[RESOURCE_HEADING_MAPPINGS[resource_name]] = values[:60]
+            out_usage[RESOURCE_HEADING_MAPPINGS[resource_name]] = values[-60:]
 
         return out_usage
 
