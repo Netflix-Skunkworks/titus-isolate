108e4325b806b965162ad442149d39971d29db1d
Gabriel Hartmann <gabriel.hartmann@gmail.com>
2020-07-02 07:50:45 -0700

    Fix startup on Mesos clusters (#288)

--- titus_isolate/kub/__init__.py
+++ titus_isolate/kub/__init__.py
@@ -2,9 +2,9 @@ import kubernetes
 
 from titus_isolate.api.testing import is_testing
 from titus_isolate.constants import DEFAULT_KUBECONFIG_PATH
+from titus_isolate.utils import is_kubernetes
 
-if is_testing():
-    core_api = None
-else:
+core_api = None
+if is_kubernetes() and not is_testing():
     core_api = kubernetes.client.CoreV1Api(
         kubernetes.config.new_client_from_config(config_file=DEFAULT_KUBECONFIG_PATH))
