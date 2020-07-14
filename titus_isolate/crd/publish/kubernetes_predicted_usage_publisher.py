9ba6131aa165e07bdda584f5e7036bdf6805bfdc
Benoit Rostykus <brostykus@netflix.com>
2020-07-14 10:38:41 -0700

    Publish resources capacity (#292)
    Publish resources capacity in CRDs

--- titus_isolate/crd/publish/kubernetes_predicted_usage_publisher.py
+++ titus_isolate/crd/publish/kubernetes_predicted_usage_publisher.py
@@ -8,6 +8,7 @@ from kubernetes.client import V1ObjectMeta, V1OwnerReference
 from kubernetes.client.rest import ApiException
 
 from titus_isolate import log
+from titus_isolate.crd.model.resources_capacity import ResourcesCapacity
 from titus_isolate.crd.model.resource_usage_prediction import ResourceUsagePredictionsResource, \
     PREDICTED_RESOURCE_USAGE_NAMESPACE, PREDICTED_RESOURCE_USAGE_NODE_NAME_LABEL_KEY, \
     PREDICTED_RESOURCE_USAGE_NODE_UID_LABEL_KEY, PREDICTED_RESOURCE_USAGE_PLURAL, CondensedResourceUsagePrediction, \
@@ -29,6 +30,7 @@ class KubernetesPredictedUsagePublisher:
                  resource_usage_predictor: ResourceUsagePredictor,
                  pod_manager: PodManager,
                  workload_monitor_manager: WorkloadMonitorManager):
+        self.__resources_capacity = ResourcesCapacity()
         self.__resource_usage_predictor = resource_usage_predictor
         self.__pod_manager = pod_manager
         self.__wmm = workload_monitor_manager
@@ -47,7 +49,7 @@ class KubernetesPredictedUsagePublisher:
                 self.__pod_manager.get_pods(),
                 self.__wmm.get_resource_usage())
 
-        condensed_predictions = CondensedResourceUsagePrediction(predictions)
+        condensed_predictions = CondensedResourceUsagePrediction(predictions, self.__resources_capacity)
 
         node = get_node()
         log.debug('owner_kind:%s owner_name:%s owner_uid:%s', node.kind, node.metadata.name, node.metadata.uid)
