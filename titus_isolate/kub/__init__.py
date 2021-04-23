63836c3c8b7dd56ed009fc2733adf91f1d19d22a
Benoit Rostykus <brostykus@netflix.com>
2021-04-23 17:12:54 +0000

    Fix buggy ordering

--- titus_isolate/kub/__init__.py
+++ titus_isolate/kub/__init__.py
@@ -5,6 +5,6 @@ from titus_isolate.constants import DEFAULT_KUBECONFIG_PATH
 from titus_isolate.utils import is_kubernetes
 
 core_api = None
-if is_kubernetes() and not is_testing():
+if (not is_testing()) and is_kubernetes():
     core_api = kubernetes.client.CoreV1Api(
         kubernetes.config.new_client_from_config(config_file=DEFAULT_KUBECONFIG_PATH))
